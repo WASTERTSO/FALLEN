6ecd6b437219e3a341fd5d6a9c70990fd25f11f9
Dipesh <83916289+DipeshxD@users.noreply.github.com>
2023-12-20 15:32:27 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -200,6 +200,7 @@ def start(update: Update, context: CallbackContext):
             elif args[0][1:].isdigit() and "rᴜʟᴇs" in IMPORTED:
                 IMPORTED["rᴜʟᴇs"].send_rules(update, args[0], from_pm=True)
         else:
+            first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
                 reply_markup=InlineKeyboardMarkup(buttons),
