d8aec36e85c3804a58981ca821ca797ef60d09da
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-11-06 10:33:45 +0530

    Update handlers.py

--- FallenRobot/modules/helper_funcs/handlers.py
+++ FallenRobot/modules/helper_funcs/handlers.py
@@ -6,7 +6,7 @@ import FallenRobot.modules.sql.blacklistusers_sql as sql
 from FallenRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "!")
+    CMD_STARTERS = ("/")
 else:
     CMD_STARTERS = "/"
 
