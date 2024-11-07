18c60839dc0adbafcc5d7403164e3bebe534cd6f
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-11-07 19:57:54 +0530

    Update misc.py

--- FallenRobot/modules/helper_funcs/misc.py
+++ FallenRobot/modules/helper_funcs/misc.py
@@ -1,7 +1,6 @@
 from math import ceil
 from typing import Dict, List
 from uuid import uuid4
-
 from telegram import (
     MAX_MESSAGE_LENGTH,
     Bot,
@@ -12,9 +11,7 @@ from telegram import (
     ParseMode,
 )
 from telegram.error import TelegramError
-
 from FallenRobot import NO_LOAD
-
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
@@ -26,11 +23,9 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
     def __gt__(self, other):
         return self.text > other.text
 
-
 def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
-
     lines = msg.splitlines(True)
     small_msg = ""
     result = []
@@ -43,21 +38,17 @@ def split_message(msg: str) -> List[str]:
     else:
         # Else statement at the end of the for loop, so append the leftover string.
         result.append(small_msg)
-
     return result
 
-
-def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
+def paginate_modules(page_n: int, module_list: List, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
-                    callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.lower()
-                    ),
+                    callback_data="{}_module({})".format(prefix, x.__mod_name__.lower()),
                 )
-                for x in module_dict.values()
+                for x in module_list
             ]
         )
     else:
@@ -65,24 +56,18 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
-                    callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.lower()
-                    ),
+                    callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower()),
                 )
-                for x in module_dict.values()
+                for x in module_list
             ]
         )
-
     pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
-
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
-
     max_num_pages = ceil(len(pairs) / 4)
     modulo_page = page_n % max_num_pages
-
     # can only have a certain amount of buttons side by side
     if len(pairs) > 3:
         pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
@@ -90,18 +75,17 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="fallen_back"),
+                EqInlineKeyboardButton(
+                    "ʜᴏᴍᴇ", callback_data="fallen_back"
+                ),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
-
     else:
         pairs += [[EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="fallen_back")]]
-
     return pairs
-
 
 def article(
     title: str = "",
@@ -117,16 +101,12 @@ def article(
         description=description,
         thumb_url=thumb_url,
         input_message_content=InputTextMessageContent(
-            message_text=message_text,
-            disable_web_page_preview=disable_web_page_preview,
+            message_text=message_text, disable_web_page_preview=disable_web_page_preview,
         ),
         reply_markup=reply_markup,
     )
 
-
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
+def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
@@ -140,64 +120,7 @@ def send_to_list(
         except TelegramError:
             pass  # ignore users who fail
 
-
 def build_keyboard(buttons):
     keyb = []
     for btn in buttons:
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
-def revert_buttons(buttons):
-    res = ""
-    for btn in buttons:
-        if btn.same_line:
-            res += "\n[{}](buttonurl://{}:same)".format(btn.name, btn.url)
-        else:
-            res += "\n[{}](buttonurl://{})".format(btn.name, btn.url)
-
-    return res
-
-
-def build_keyboard_parser(bot, chat_id, buttons):
-    keyb = []
-    for btn in buttons:
-        if btn.url == "{rules}":
-            btn.url = "http://t.me/{}?start={}".format(bot.username, chat_id)
-        if btn.same_line and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
-        else:
-            keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
-
-    return keyb
-
-
-def user_bot_owner(func):
-    @wraps(func)
-    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
-        user = update.effective_user
-        if user and user.id == OWNER_ID:
-            return func(bot, update, *args, **kwargs)
-        else:
-            pass
-
-    return is_user_bot_owner
-
-
-def build_keyboard_alternate(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn[2] and keyb:
-            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
-        else:
-            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
-
-    return keyb
-
-
-def is_module_loaded(name):
-    return name not in NO_LOAD
+        if btn.same_line and
